38dda6e58032d29fff7e9fb3898ff7797226dc59
Ankit Singh <singhankit0033@gmail.com>
2025-03-03 03:50:06 +0530

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+// let score = "33abc";
+// let score = null;
+// let score = undefined;
+let score = false;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 / false => 0
+
+let isLoggedIn = 1;
+let isBooleanLoggedIn = Boolean(isLoggedIn);
+console.log(isBooleanLoggedIn);
+
+// 1 => true / 0 => false 
+// "" => false
+// "ankit" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+
+
+
+
