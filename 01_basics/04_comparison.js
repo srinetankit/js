fcd2d26cf81812e72c581886d8f6d632ee48e139
Ankit Singh <singhankit0033@gmail.com>
2025-03-03 14:21:33 +0530

    comparsion operators

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,11 @@
+// null value converts to 0 only while doing the comparison, if doing the eqility check null will still be null. 
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+console.log("2" == 2);
+console.log("2" === 2);
