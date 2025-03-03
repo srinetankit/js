2556eaeebf69843049035a788c9b4cc6bce5fd25
Ankit Singh <singhankit0033@gmail.com>
2025-03-03 14:19:23 +0530

    conversion operators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -17,7 +17,7 @@ let valueInNumber = Number(score);
 
 let isLoggedIn = 1;
 let isBooleanLoggedIn = Boolean(isLoggedIn);
-console.log(isBooleanLoggedIn);
+// console.log(isBooleanLoggedIn);
 
 // 1 => true / 0 => false 
 // "" => false
@@ -25,8 +25,40 @@ console.log(isBooleanLoggedIn);
 
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof(stringNumber));
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+
+
+// ********************************** Operations ************************************
+
+let value = 3;
+let negValue = -value;
+// console.log(value);
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+let str1 = "Hello ";
+let str2 = "ankit";
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log((3 + 4) * 5 % 3);
+
+let numCounter = 100;
+numCounter++;
+console.log(numCounter);
+
+
 
 
 
